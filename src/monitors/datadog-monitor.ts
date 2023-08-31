5076df94ea9587e2346f499eaeb76145eae67b4f
Gabriel Furstenheim <gabriel.f@goodnotes.com>
2023-08-31 16:32:02 +0200

    Upgrade datadog monitor version

--- src/monitors/datadog-monitor.ts
+++ src/monitors/datadog-monitor.ts
@@ -2,37 +2,10 @@ import { CfnResource } from 'aws-cdk-lib';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 import camelcaseKeys = require('camelcase-keys');
 import { Construct } from 'constructs';
-import { DatadogCredentials } from '../common/properties';
-import { MonitorOptions, MonitorType } from './properties';
-
-export interface DatadogMonitorProps {
-  /** Credentials for the Datadog API */
-  readonly datadogCredentials: DatadogCredentials;
-
-  /** The monitor query */
-  readonly query: string;
-
-  /** The type of the monitor */
-  readonly type: MonitorType;
-
-  /** A message to include with notifications for the monitor*/
-  readonly message?: string;
-
-  /** Name of the monitor */
-  readonly name?: string;
-
-  /** Tags associated with the monitor */
-  readonly tags?: string[];
-
-  /** The monitor options*/
-  readonly options?: MonitorOptions;
-
-  /** Whether or not the monitor is multi alert */
-  readonly multi?: boolean;
-}
+import { DatadogMonitorProps } from '../../lib';
 
 /**
- * Datadog Monitor 3.0.0
+ * Datadog Monitor 4.6.0
  */
 export class DatadogMonitor {
   constructor(scope: Construct, id: string, props: DatadogMonitorProps) {
--- src/monitors/properties.ts
+++ src/monitors/properties.ts
@@ -2,8 +2,10 @@
  * The type of the monitor
  */
 export enum MonitorType {
+  AUDIT_ALERT = 'audit alert',
   COMPOSITE = 'composite',
   EVENT_ALERT = 'event alert',
+  EVENT_V_2_ALERT = 'event-v2 alert',
   LOG_ALERT = 'log alert',
   METRIC_ALERT = 'metric alert',
   PROCESS_ALERT = 'process alert',
@@ -11,6 +13,11 @@ export enum MonitorType {
   SERVICE_CHECK = 'service check',
   SYNTHETICS_ALERT = 'synthetics alert',
   TRACE_ANALYTICS_ALERT = 'trace-analytics alert',
+  SLO_ALERT = 'slo alert',
+  RUM_ALERT = 'rum alert',
+  CI_PIPELINES_ALERT = 'ci-pipelines alert',
+  ERROR_TRACKING_ALERT = 'error-tracking alert',
+  CI_TESTS_ALERT = 'ci-tests alert'
 }
 
 export interface MonitorOptions {
